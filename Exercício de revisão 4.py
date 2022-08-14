1e2771e6d46d6e76225fce5b855fcffe1531e389
Derek WIllian <derekoob@hotmail.com>
2022-08-14 17:09:18 -0300

    Versão 1

---
+++ Exercício de revisão 4.py
@@ -0,0 +1,9 @@
+def media (a,b,c):
+    m = (a+b+c)/3
+    return m
+
+A = float(input('Insira um valor: '))
+B = float(input('Insira um segundo valor: '))
+C = float(input('Insira um terceiro valor: '))
+
+print(media(A,B,C))
